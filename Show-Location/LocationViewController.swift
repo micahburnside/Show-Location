687ea2cf993f1342027abe48dedaf1d12132a6b6
Micah <micahburnside@protonamil.com>
2022-04-10 16:45:20 -0400

    Adds and sets locationsArray to empty at build time. Adds Moving Map functionality with userTrackingMode set to true with heading tracking.

--- Show-Location/LocationViewController.swift
+++ Show-Location/LocationViewController.swift
@@ -10,23 +10,29 @@ import MapKit
 import CoreLocation
 
 class LocationViewController: UIViewController, MKMapViewDelegate {
-
+    var locationsArray: [CLLocation]? = []
     let mapView = MKMapView()
     var regionInMeters: Double = 250
-    private var currentLocation: CLLocation?
+    var currentLocation: CLLocation?
     private let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view = mapView
+        setupMapView()
         setupLocationManager()
         getDeviceLocation()
         centerViewOnUserLocation()
+
     }
     override func viewDidAppear(_ animated: Bool) {
-        print(currentLocation?.coordinate.latitude)
-        print(currentLocation?.coordinate.longitude)
+//        takeLocationSnapShot()
+    }
+    
+    func setupMapView() {
+        mapView.delegate = self
+        self.view = mapView
+        self.mapView.showsCompass = true
+        self.mapView.mapType = .hybrid
     }
     func getDeviceLocation() {
         mapView.delegate = self
@@ -66,6 +72,23 @@ class LocationViewController: UIViewController, MKMapViewDelegate {
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Swift.Error) {
         print("error getting user location, error: \(error.localizedDescription)")
     }
+    
+    func takeLocationSnapShot() {
+        let timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { Timer in
+            self.takeLocationSnapShot()
+        }
+        locationsArray?.append(currentLocation!)
+        print(self.locationsArray)
+        print(self.locationsArray?.count )
+        if self.locationsArray?.count ?? 10 == 10 {
+            timer.invalidate()
+            print("locationsArray.count has reached 10 locations: \(String(describing: locationsArray))")
+        }
+    }
+    
+    func takeSnaphotAtInterval() {
+
+    }
 }
 
 // MARK: - CLLocationManagerDelegate
@@ -86,6 +109,9 @@ extension LocationViewController: CLLocationManagerDelegate {
       _ manager: CLLocationManager,
       didUpdateLocations locations: [CLLocation]
     ) {
+//                self.mapView.setUserTrackingMode(.follow, animated:true)
+
+        self.mapView.setUserTrackingMode(.followWithHeading, animated: true)
         defer { currentLocation = locations.last }
 
         if currentLocation == nil {
@@ -94,6 +120,7 @@ extension LocationViewController: CLLocationManagerDelegate {
 //                let viewRegion = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
                 let span = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
                 let region = MKCoordinateRegion(center: userLocation.coordinate, span: span)
+                
                 mapView.setRegion(region, animated: true)
             }
         }
